372303ff79481f7c080f812dbbf0b520bcffa93b
nlw113 <nathanworford@gmail.com>
2017-11-30 10:14:53 -0700

    game working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,95 @@
 
 
-var goal;
-
-/*
-var sum = function(a, b)
-{
-	return a + b;
-}
-*/
+var goal_max = 120; //target points to win (120 max, 19 minimum)
+var goal_min = 19;
+var goal = 0; //no particular goal to start out with
 
 var button_points = [0, 0, 0, 0, 0];
 var points = 0;
-
-$(document).ready(function() {
-	var goal = Math.floor(Math.random() * 102) + 19;
-	
-	$("#your-random-target").html(goal);
-
-	/* var x = sum(1, 2); */
-	for(var i=1;i<=4;i++)
-	{
-		button_points[i] = 1 + Math.floor(Math.random() * 10);
-		console.log("Setting up button " + i + " for " + button_points[i] + " points");
-	
-		$("#button-"+i).click(function(){
-			var button_name = $(this).attr('id');
-			console.log("Reading: " + button_name);
-			var button_num = button_name[button_name.length-1];
-			console.log("Button number; " + button_num);
-			console.log("You got " + button_points[button_num] + " points");
-			points = points + button_points[button_num];
-			$("#your-score-points").html(points);
-		});
-	};
-
-//reset game
-var reset = {
-
-}
-
-
-// when button is clicked, add value to points
-
-$("#button-"+i).click(function(){
-
-$('#your-score-points') = $('#your-score-points') + button_points[i];
-
-$('#your-score-points').html(points);
-
-};
-
+var losses = 0;
+var wins = 0;
 
 var buildingFinished = false;
 var buildingFailed = false;
 
-if ($('#your-score-points') == $('#your-random-target')) { 
-
-	$('buildingFinished') = true;
-
-	console.log('You win!');
-
-	$('#wins').html();
-
-
-};
-
-if ($('#your-score-points') > $('#your-random-target')) { 
-
-	$('#buildingFailed') = true;
-
-	console.log('You lose!');
-
-	$('#losses')++;
-
-	$('#losses').html();
-
-};
-
-
-else () {
-
-	console.log('game is not over yet')
-};
-
-if ($('buildingFailed') == true) { 
-	reset()
-};
-
-if ($('buildingFinished') == true) { 
-	reset()
-};
-
-else () {
-	console.log('game is not over yet!');
+var setupGame = function() {
+	buildingFinished = false;
+	buildingFailed = false;
+	points = 0;
+	//pick a goal
+	//.        round down.random 0.0-0.99 times (range+1) to generate 0-range, plus minimum
+	goal = Math.floor(Math.random() * (goal_max-goal_min+1)) + goal_min;
+	for(var i=1;i<=4;i++) // 4 buttons: 1, 2, 3, and 4
+	{
+		//next "points" is between 1 and 10 (1 + random 0.0-0.9999 * 10)
+		button_points[i] = 1 + Math.floor(Math.random() * 10);
+		console.log("Setting up button " + i + " for " + button_points[i] + " points");
+	}
 }
 
+var refreshPoints = function() { 
+	$("#your-random-target").html(goal); //put the goal on the screen
+	$("#your-score-points").html(points);
+}
+
+var refreshDisplay = function() {
+	$('#wins').html(wins);
+	$('#losses').html(losses);
+
+}
+
+//checks if game over (capable of resetting entire game if so)
+var checkGame = function() {
+	if (points == goal) { 
+		buildingFinished = true;
+		console.log('You win!');
+		wins++;
+
+		$('#lose_div').html("You completed the skyscraper!");
+
+		setupGame();
+	}
+	else if (points > goal) { 
+		buildingFailed = true;
+		console.log('You lose!');
+		losses++;
+
+		$('#lose_div').html("Your skyscraper failed!! ;-;");
+
+		setupGame();
+	}
+	else {
+		console.log('game is not over yet')
+		$('#lose_div').html("Good Luck!");
+	}
+}
+
+$(document).ready(function() {
+	
+	//wire up all the buttons
+	for(var i=1;i<=4;i++) // 4 buttons: 1, 2, 3, and 4
+	{
+		//get the next button ("#button-X"), where X is 1, 2, 3, o 4
+		$("#button-"+i).click(function(){ //when that button (any button) is clicked:
+			//get the ID of the button (button-1, button-2, button-3, button-4)
+			var button_name = $(this).attr('id');
+			//get the number off the button (last letter of the button name)
+			var button_num = button_name[button_name.length-1];
+			//get the number of new points that button is worth
+			var points_earned = button_points[button_num];		
+			points = points + points_earned;
+			//$('#your-score-points') = $('#your-score-points') + button_points[i];
+
+			checkGame();
+			refreshPoints(); //ALWAYS refresh points
+			refreshDisplay(); //stuff changed, update the screen
+		});
+	};
+
+	//start the first game
+	setupGame();
+	refreshPoints(); //ALWAYS refresh points
+	refreshDisplay();
 });
 
 // $('button-1') $('button-2')  $('button-3')  $('button-4')
